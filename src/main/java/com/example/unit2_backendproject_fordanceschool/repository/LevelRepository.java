0e4bb1a03acea7ec5c741bea1eaf7a049221f8c6
suganthi1410 <suganthi1410@gmail.com>
2026-09-07 12:11:41 -0500

    created LevelRepository inside the repository package for interface

---
+++ src/main/java/com/example/unit2_backendproject_fordanceschool/repository/LevelRepository.java
@@ -0,0 +1,9 @@
+package com.example.unit2_backendproject_fordanceschool.repository;
+
+import com.example.unit2_backendproject_fordanceschool.model.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//creating repository for level table
+    public interface LevelRepository extends JpaRepository<Level, Long> {
+    }
+
