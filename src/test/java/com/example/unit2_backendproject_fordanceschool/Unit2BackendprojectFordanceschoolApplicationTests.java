9f788842dd0ae3ac16946eb02de22d5fd3ebbdaf
suganthi1410 <suganthi1410@gmail.com>
2026-09-06 21:14:26 -0500

    Initialcommit_created new java/springbootapp with basic dependencies

---
+++ src/test/java/com/example/unit2_backendproject_fordanceschool/Unit2BackendprojectFordanceschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit2_backendproject_fordanceschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2BackendprojectFordanceschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
