1bcf749055d17c89d1be35e7914cd046f08d3e68
suganthi1410 <suganthi1410@gmail.com>
2026-09-11 16:06:47 -0500

    created service and controller to check user login successful or failed,added predefined username and password

---
+++ src/main/java/com/example/unit2_backendproject_fordanceschool/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.unit2_backendproject_fordanceschool.service;
+
+import com.example.unit2_backendproject_fordanceschool.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean login(String username, String password) {
+        return userRepository.findByUsername(username)
+                .map(user -> user.getPassword().equals(password))
+                .orElse(false);
+    }
+}
