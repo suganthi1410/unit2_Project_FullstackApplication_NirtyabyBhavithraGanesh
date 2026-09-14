afd780992e514784dc16aba874acb9333cf37d85
suganthi1410 <suganthi1410@gmail.com>
2026-09-14 18:40:42 -0500

    refactored reviewlist to fetch/GET all review

--- frontend/src/components/reviewpage/ReviewList.jsx
+++ frontend/src/components/reviewpage/ReviewList.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import ReviewCard from "./ReviewCard";
 
-function ReviewList({ reviews }) {
+function ReviewList() {
+  const [reviews, setReviews] = useState([]);
+   useEffect(() => {
+    fetch("http://localhost:8080/reviews")
+      .then((response) => response.json())
+      .then((data) => setReviews(data))
+      .catch((error) => console.error("Error fetching reviews:", error));
+  }, []);
   return (
     <div className="review-list">
       {reviews.map((review) => (
