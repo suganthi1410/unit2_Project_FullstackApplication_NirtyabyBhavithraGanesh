8ca2c7b881b5a8a1877d216d294e2520ee1d0805
suganthi1410 <suganthi1410@gmail.com>
2026-09-14 12:34:05 -0500

    created modal component and integrated that into reviewpage

--- frontend/src/components/reviewpage/ReviewPage.jsx
+++ frontend/src/components/reviewpage/ReviewPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
+import Modal from "../modal/Modal";
 
 function ReviewPage() {
     /*
@@ -32,15 +33,34 @@ function ReviewPage() {
   ];
   */
  const [reviews, setReviews] = useState([]);
+ const [showModal, setShowModal] = useState(false);
+ const [editingReview, setEditingReview] = useState(null);
+
  useEffect(() => {
     fetch("http://localhost:8080/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data))
       .catch((err) => console.error("Error fetching reviews:", err));
   }, []);
+
+  
   return (
     <main>
-      <h2>Student Reviews</h2>
+      <h2>Student Reviews</h2>      
+      <button
+        onClick={() => {
+          setEditingReview(null);   
+          setShowModal(true);       
+        }}
+      >
+        Write a Review
+      </button>
+     
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          {/* ReviewForm will be added here next */}
+        </Modal>
+      )}
       <ReviewList reviews={reviews} />
     </main>
   );
