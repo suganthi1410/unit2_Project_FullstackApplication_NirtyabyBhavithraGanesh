4172f7ae0a910790d0a47e49cb569a541f96d4b9
suganthi1410 <suganthi1410@gmail.com>
2026-09-14 14:07:36 -0500

    added level field input in ReviewForm

--- frontend/src/components/reviewpage/ReviewForm.jsx
+++ frontend/src/components/reviewpage/ReviewForm.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+
 function ReviewForm({ onClose, onSave, editingReview }) {
     const [username, setUsername] = useState("");
+    const [levelName, setLevelName] = useState("");
   return (
     <form className="review-form">
-      <h3>{editingReview ? "Edit Review" : "Add Review"}</h3>
+      <h3>{editingReview ? "Edit Review" : "Add Review"}</h3>  
+      <label>Name</label>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      /> 
+      <label>Level</label>
+      <select
+        value={levelName}
+        onChange={(e) => setLevelName(e.target.value)}
+        required
+      >
+        <option value="">Select level</option>
+        <option value="Beginner">Beginner</option>
+        <option value="Intermediate">Intermediate</option>
+        <option value="Advanced">Advanced</option>
+      </select>   
     </form>
   );
 }
