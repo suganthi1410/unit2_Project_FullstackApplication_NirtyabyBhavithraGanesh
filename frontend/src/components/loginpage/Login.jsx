c23c0bd23608a946dea974714354673faa7f5eb3
suganthi1410 <suganthi1410@gmail.com>
2026-09-14 22:50:23 -0500

    error handling added for empty input fields in login

--- frontend/src/components/loginpage/Login.jsx
+++ frontend/src/components/loginpage/Login.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 function LoginPage(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    
+    //error handling for empty username password
+    function handleSubmit(e) {
+    e.preventDefault();
 
+    if (!username || !password) {
+      setError("Username and password are required")
+      return;    }
+
+    console.log("Login clicked:", username, password);
+  }
     return(
     <div>
         <h2>Login</h2>
-        <form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <form onSubmit={handleSubmit}>
         <label>Username</label>
         <input 
         type="text" 
