1f8b217dc4c54530ea58148825c76a69619ec724
suganthi1410 <suganthi1410@gmail.com>
2026-09-17 20:21:03 -0500

    hamburger added to header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,18 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 import logo from '../../assets/images/logo.jpeg';
+
 
 //function created for header that displays Logo,
 //page title and links to other pages 
 
 function Header({ loggedInUser , onLogout }) {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <header className="header">
             <div className="logo-title">
                 <img src={logo} alt="Nirtya LOGO" width="100" className="logo" />
                 <h1 className="title">Nirtya by Bhavithra Ganesh</h1>
+                       
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            {"\u2630"}
             </div>
 
-            <nav className="header-nav" >
+            </div>
+
+            <nav className={`header-nav ${menuOpen ? "open" : ""}`}>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/class"  >Class</Link>
